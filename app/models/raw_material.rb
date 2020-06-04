cfd7671c0658656970dd438da9162779f7397b92
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-04 23:59:07 +0000

    add seed data

--- app/models/raw_material.rb
+++ app/models/raw_material.rb
@@ -1,3 +1,3 @@
 class RawMaterial < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 yvonne = User.create(username:"yvonne", password: "password")
 nguyen = User.create(username:"nguyen", password: "password")
+
+toluene = RawMaterial.create(:chemical => "Toluene", :company => "Allnex", :lot_number => "SHBJ128391", :amount => 50, :user_id => yvonne.id)
+iboa = RawMaterial.create(:chemical => "IBOA", :company => "Allnex", :lot_number => "IBOA2930812", :amount => 100, :user_id => nguyen.id)
+
+yvonne.raw_materials.create(:chemical => "Hexane")
+
+toluene.user.create(:user_id => yvonne.id)
