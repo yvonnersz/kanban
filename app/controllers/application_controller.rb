4d01b9c9f13e578fa85f1938f4e136d6d5e29d0a
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-05 01:54:01 +0000

    add content for welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    if logged_in?
-      redirect "users/#{current_user.id}"
-    else
-      erb :welcome
-    end
+    erb :welcome
   end
 
   helpers do
