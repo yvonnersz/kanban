f562742cc2dcb71174ffb74be74f6150e3140e43
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-05 01:30:24 +0000

    add raw_materials edit route and view

--- app/controllers/raw_materials_controller.rb
+++ app/controllers/raw_materials_controller.rb
@@ -19,6 +19,20 @@ class RawMaterialsController < ApplicationController
     erb :'/raw_materials/show'
   end
 
+  get '/raw_materials/:id/edit' do
+    @raw_material = RawMaterial.find_by(:id => params[:id])
+    erb :'raw_materials/edit'
+  end
+
+  patch '/raw_materials/:id' do
+    @raw_material = RawMaterial.find_by(:id => params[:id])
+    @raw_material.update(:chemical => params[:chemical],
+                         :company => params[:company],
+                         :lot_number => params[:lot_number],
+                         :amount => params[:amount])
+    redirect "/raw_materials/#{@raw_material.id}"
+  end
+
 
 
 end
