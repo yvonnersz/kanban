33bc0bf819b7e539aff93b72a6b8811ac6cf68dc
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-09 05:47:29 +0000

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 yvonne = User.create(username:"yvonne", password: "password")
 nguyen = User.create(username:"nguyen", password: "password")
 
-toluene = RawMaterial.create(:chemical => "Toluene", :company => "Allmex", :lot_number => "TOLU060820", :amount => 50, :user_id => yvonne.id)
-iboa = RawMaterial.create(:chemical => "IBOA", :company => "Allmex", :lot_number => "IBOA062020", :amount => 100, :user_id => nguyen.id)
+toluene = RawMaterial.create(:chemical => "Toluene", :company => "Allrex", :lot_number => "TOLU060820", :amount => 50, :user_id => yvonne.id)
+iboa = RawMaterial.create(:chemical => "IBOA", :company => "Allrex", :lot_number => "IBOA062020", :amount => 100, :user_id => nguyen.id)
 
 toluene.user.create(:user_id => yvonne.id)
 iboa.user.create(:user_id => nguyen.id)
