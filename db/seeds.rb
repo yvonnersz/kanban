6f344b4d268e29aeda139b9674e8dc73e146399b
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-09 23:46:40 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 yvonne = User.create(username:"yvonne", password: "password")
-nguyen = User.create(username:"nguyen", password: "password")
+simon = User.create(username:"simon", password: "password")
 
 toluene = RawMaterial.create(:chemical => "Toluene", :company => "Allrex", :lot_number => "TOLU060820", :amount => 50, :user_id => yvonne.id)
-iboa = RawMaterial.create(:chemical => "IBOA", :company => "Allrex", :lot_number => "IBOA062020", :amount => 100, :user_id => nguyen.id)
+iboa = RawMaterial.create(:chemical => "IBOA", :company => "Allrex", :lot_number => "IBOA062020", :amount => 100, :user_id => simon.id)
 
 toluene.user.create(:user_id => yvonne.id)
-iboa.user.create(:user_id => nguyen.id)
+iboa.user.create(:user_id => simon.id)
 
 yvonne.raw_materials.create(:chemical => "Toluene", :company => "Allmex", :lot_number => "TOLU060820", :amount => 50, :user_id => yvonne.id)
-nguyen.raw_materials.create(:chemical => "IBOA", :company => "Allmex", :lot_number => "IBOA062020", :amount => 100, :user_id => nguyen.id)
+simon.raw_materials.create(:chemical => "IBOA", :company => "Allmex", :lot_number => "IBOA062020", :amount => 100, :user_id => simon.id)
