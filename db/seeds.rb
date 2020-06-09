41e85b676279ee823cdeff8583d1feea2267b61b
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-09 00:29:06 +0000

    fix raw_mat index page to show user submission

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 yvonne = User.create(username:"yvonne", password: "password")
 nguyen = User.create(username:"nguyen", password: "password")
 
-toluene = RawMaterial.create(:chemical => "Toluene", :company => "Allnex", :lot_number => "SHBJ128391", :amount => 50, :user_id => yvonne.id)
-iboa = RawMaterial.create(:chemical => "IBOA", :company => "Allnex", :lot_number => "IBOA2930812", :amount => 100, :user_id => nguyen.id)
-
-yvonne.raw_materials.create(:chemical => "Hexane")
+toluene = RawMaterial.create(:chemical => "Toluene", :company => "Allmex", :lot_number => "TOLU060820", :amount => 50, :user_id => yvonne.id)
+iboa = RawMaterial.create(:chemical => "IBOA", :company => "Allmex", :lot_number => "IBOA062020", :amount => 100, :user_id => nguyen.id)
 
 toluene.user.create(:user_id => yvonne.id)
+iboa.user.create(:user_id => nguyen.id)
+
+yvonne.raw_materials.create(:chemical => "Toluene", :company => "Allmex", :lot_number => "TOLU060820", :amount => 50, :user_id => yvonne.id)
+nguyen.raw_materials.create(:chemical => "IBOA", :company => "Allmex", :lot_number => "IBOA062020", :amount => 100, :user_id => nguyen.id)
